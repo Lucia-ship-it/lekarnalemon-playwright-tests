3648f7f356434e7992441e2907c755ac3575cc99
Lucia_ship_it <lucia.kobzova@icloud.com>
2025-08-15 15:59:54 +0200

    final upravy, zmeny 2 lokatorov

--- test_projekt1.py
+++ test_projekt1.py
@@ -1,3 +1,4 @@
+from playwright.sync_api import sync_playwright
 from playwright.sync_api import Page
 from datetime import datetime
 import pytest
@@ -6,28 +7,22 @@ import pytest
 def page():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False, slow_mo=2000)
-        # context = browser.new_context()
-        # page = context.new_page()
         page = browser.new_page()
         yield page
 
         page.close()
         browser.close()
 
-def accept_cookies(page: Page):
-    try:
-        button = page.locator("#cookiescript_accept")
-        if button.is_visible():
-            button.click()
-            page.wait_for_timeout(1000)
-    except:
-        pass  
+# def accept_cookies(page: Page):
+#     try:
+#         button = page.locator("#cookiescript_accept")
+#         if button.is_visible():
+#             button.click()
+#             page.wait_for_timeout(1000)
+#     except:
+#         pass  
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-
-#pytest test_projekt.py
-# DEBUG
-
 
 
 def test_title_lekarna(page: Page):
@@ -41,25 +36,20 @@ def test_cookies_click(page: Page):
     page.goto("https://www.lekarnalemon.cz/")
     button = page.locator("#cookiescript_accept") 
     button.click()
-    page.wait_for_timeout(2000)
-
-    print("after click")
+    page.wait_for_timeout(1000)
     cookies_square = page.locator("#cookiescript_injected")
     
     assert cookies_square.is_visible() == False
 
-def test_new_page(page: Page):
+def test_dropdown_menu(page: Page):
     page.goto("https://www.lekarnalemon.cz/")
-    accept_cookies(page)
-    # na stranke najdi v menu leto a klikni nan
+    #accept_cookies(page)
     button = page.locator("#desktop-a8ab5f3c-86fa-414f-999e-4601782baaec-dropdown")
     button.click()
-
-    # rozbali sa mi ponuka menu, screenshot  
-    page.wait_for_selector("body > header > div.page-header__navigation > div > nav > div.menu-item.menu-item-dropdown.show > div > div", 5000)
+  
+    page.wait_for_selector("body > header > div.page-header__navigation > div > nav > div.menu-item.menu-item-dropdown.show > div > div", timeout=2000)
     page.screenshot(path="ponuka menu.png")
 
-    # klik na text s doplnkami stravy:
     h_text = page.locator("text=Doplňky stravy na opalování")
     h_text.click()
     print("po kliknuti na doplnky stravy")
@@ -69,13 +59,10 @@ def test_new_page(page: Page):
 
 def test_cart(page: Page):
     page.goto("https://www.lekarnalemon.cz/leto/doplnky-stravy-na-opalovani")
-    accept_cookies(page)
-
+    #accept_cookies(page)
     label = page.locator("label:has-text('GS')")
-    input_id = label.get_attribute("for")
-    checkbox = page.locator(f"input#{input_id}") #check len cez input
-    checkbox.check()
-    checkbox.wait_for(state="visible")
+    label.click()
+    checkbox = page.locator("input:checked")
 
     assert checkbox.is_checked(), "Checkbox pre GS nie je zaškrtnutý"
 
@@ -85,21 +72,13 @@ def test_cart(page: Page):
 
     assert page.url ==  "https://www.lekarnalemon.cz/leto/doplnky-stravy-na-opalovani/gs-betakaroten-gold-15mg-cps-30-8595693300541"
 
-
     print("pokracujeme na novej stranke")
     page.wait_for_load_state("networkidle")
-
-    buy_button = page.locator("#product-head div.js-buy-button > button") #snad ho najde
-    # buy_button = page.locator("#product-head > div.container.mt-lg-4 > form > div > div:nth-child(2) > div > div > div > div.col-xl-7.col-lg-10.col-sm-9.col-md-6 > div > div.col-6.js-buy-button > button")
+    buy_button = page.locator("#product-head > div.container.mt-lg-4 > form > div > div:nth-child(2) > div > div > div > div.col-xl-7.col-lg-10.col-sm-9.col-md-6 > div > div.col-6.js-buy-button > button")
     buy_button.click()
-    print("kliklo")
-    page.wait_for_selector("#modal-template-content > div.modal__body", 5000)
-
-    # presun do kosiku
+    page.wait_for_selector("#modal-template-content > div.modal__body",timeout=5000)
     cart = page.locator("#modal-template-content div.cart-modal-buttons a")
-    # stare - ("#modal-template-content > div.modal__body > div.row.cart-modal-buttons > div.col-sm-6.text-center.text-sm-right.order-sm-1.mb-3.mb-sm-0 > a")
     cart.click()
-    print("kliklo na pokracovat do kosiku")
  
     page.wait_for_url("https://www.lekarnalemon.cz/kosik")
     print("sme v kosiku")
@@ -107,20 +86,15 @@ def test_cart(page: Page):
     # skontrolujem, ze sa mi tam nachadza produkt
     cart_items = page.locator("#cart-form")
 
-    assert cart_items.filter(has_text="Betakaroten").first.is_visible()
-
-#toto nie je dobre 
-    # assert cart_content.filter(has_text="Betakaroten").is_visible()
+    assert cart_items.filter(has_text="Betakaroten").is_visible()
 
 
 def test_log_icon(page : Page):
-    '''chcem zistit jestli je videt text ri ikonke prihlasit sa
-'''
     page.goto("https://www.lekarnalemon.cz/")
-    accept_cookies(page)
+    #accept_cookies(page)
     icon = page.locator("body > header > div.container > div > div.page-header__top-nav > a:nth-child(2) > span.page-header__top-link--icon")
     icon.hover(timeout=1000) 
-    #kontrola na objavenie sa textu
+
     text = page.locator('span.page-header__top-link--text', has_text="Přihlásit")
 
     assert text.is_visible(), "Text 'Přihlásit' sa nezobrazil po hovernutí na ikonku"
@@ -128,7 +102,7 @@ def test_log_icon(page : Page):
 
 def test_log_in_negative(page: Page):
     page.goto("https://www.lekarnalemon.cz/login")
-    accept_cookies(page)
+    #accept_cookies(page)
     page.fill("#_username", "test@email.com")
     page.fill("#_password", "heslo")
     page.press("#_password", "Enter")
@@ -141,13 +115,13 @@ def test_log_in_negative(page: Page):
 
 def test_new_page(page: Page):
     page.goto("https://www.lekarnalemon.cz/")
-    accept_cookies(page)
+    #accept_cookies(page)
 
     with page.expect_popup() as popup:
         button = page.locator("footer .page-footer__menu a:nth-child(1) picture img")
         button.click()
 
         new_page = popup.value
-        assert new_page == "https://prehledy.sukl.cz/prehledy.html#/lekarny/00215013336?verify=true"
+        assert new_page.url == "https://prehledy.sukl.cz/prehledy.html#/lekarny/00215013336?verify=true"
 
 
